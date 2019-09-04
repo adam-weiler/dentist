759a30285ee8ce8a5a7ad33f67665aabf954ab49
adam-weiler <adam.weiler@yahoo.ca>
2019-09-04 10:21:51 -0400

    Starting a new project, adding home, contact, procedures components.

---
+++ src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+    return(
+        <>
+            <h1>Dentist Office</h1>
+            <p>Welcome to the Dentist office!</p>
+        </>
+    )
+}
+
+export default Contact;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <>
+            <h1>Dentist Office</h1>
+            <p>Welcome to the Dentist office!</p>
+        </>
+    )
+}
+
+export default Home;
