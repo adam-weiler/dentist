59ae15ba5f6f469d1cac1457bdd3ff1b395d3811
adam-weiler <adam.weiler@yahoo.ca>
2019-09-04 10:34:07 -0400

    Using swtich on routers.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Contact from './pages/Contact';
@@ -12,14 +12,22 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path='/' component={Home} />
-        <Route path='/procedures' component={Procedures} />
-        <Route path='/contact' component={Contact} />
-{/* 
-        <Home />
-        <Contact/>
-        <Procedures/> */}
 
+        <nav>
+
+          
+        </nav>
+
+        <Switch>
+        {/* <Route exact path='/' component={Home} /> */}
+          <Route path='/procedures' component={Procedures} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/' component={Home} />
+  {/* 
+          <Home />
+          <Contact/>
+          <Procedures/> */}
+        </Switch>
 
       </div>
     </Router>
